e9a4add760ef23f72b6caabc66632c11842be81a
kevser <kai.kailishan@gmail.com>
2018-05-09 01:45:16 -0500

    adding more demo

--- src/test/java/Academy/Demo.java
+++ src/test/java/Academy/Demo.java
@@ -7,5 +7,8 @@ public class Demo {
 		System.out.println("Demo1");
 		System.out.println("Demo2");
 		System.out.println("Demo3");
+		System.out.println("Demo4");
+		System.out.println("Demo5");
+		System.out.println("Demo6");
 	}
 }
